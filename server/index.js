b997ccd12358dcbba8c1d290d05d7354cbf17cfe
Brandon Bunch <brandonbunch@Brandons-MacBook-Pro.local>
2020-10-17 14:37:49 -0500

    pushing for cameron

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
 const cors = require('cors'); // Using the CORS node package because cors is a pain to use, so just use the package instead of fighting with it
+const pool = require('./db');
 
 app.use(cors()); // implements the cors package middleware
 app.use(express.json()); // You have to use this to get the body through your requests, otherwise it wont be available to use
 
-let dbId;
-let dbName;
-
-app.route('/api/updateName').put((req, res) => {
-	const { name, id } = req.body; // destructures name and id off of body, just saves code
-	dbId = id;
-	dbName = name;
-	res.status(200).send({ id: dbId, name: dbName }); // sends 200 and the result of the update
+app.route('/demoForCameron').get((req, res) => {
+	pool.query('SELECT * FROM accounts', (err, users) => {
+		if (err) {
+			console.log(err);
+			// send the SQL error if something goes wrong
+			res.status(500).json({ err });
+		} else {
+			res.status(200).send({ peeps: users.rows });
+		}
+	});
 });
 
 app.listen(8675, () => console.log('Listening on port 8675')); // starts the server so you can hit your API's
